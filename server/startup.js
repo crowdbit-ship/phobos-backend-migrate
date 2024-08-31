4fba35aa540b65a1039d2cf15d09658cc51675ee
Po Buchi <PoBuchi@users.noreply.github.com>
2024-08-31 13:42:07 +0700

    Update startup.js

--- server/startup.js
+++ server/startup.js
@@ -1,9 +1,19 @@
 import { Meteor } from 'meteor/meteor'
 import { check } from 'meteor/check'
 import { db } from '/server/lib/collections/server/db'
+import { checkCreateApp } from '/server/service/app/app.helper'
 
 export let DEV_ENV = false
 
+function setupProdEnviroment() {
+  const firstRun = checkCreateApp()
+  if (firstRun) {
+    console.log('🥭','Doing firstRun')
+    console.log(db.App.find().fetch())
+  }
+}
+
 Meteor.startup(() => {
   console.log('🪷', 'Started')
+  setupProdEnviroment()
 })
